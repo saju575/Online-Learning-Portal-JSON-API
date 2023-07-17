900ca622f9292a01641b3ffa987ab1a036911686
saju575 <sakilahmedsaju575@gmail.com>
2023-07-17 21:50:45 +0600

    change in base route

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.db = router.db;
-
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
 server.use(auth);
 
-server.use("/api/v1", router); // You can change the base URL here, e.g., '/api'
+server.use("/", router); // You can change the base URL here, e.g., '/api'
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
